8dbc14bec13ba08cc75f19a4d8c0764ee144be41
Harry <hk@hk-singh.com>
2021-04-30 14:23:42 +0200

    DOM

---
+++ myscript.js
@@ -0,0 +1,13 @@
+var headOne = document.querySelector('#one')
+var headTwo = document.querySelector('#two')
+var headThree = document.querySelector('#three')
+
+headOne.addEventListener('mouseover', function(){
+    headOne.textContent = "Mouse Currently Over";
+    headOne.style.color = 'red';
+})
+
+headOne.addEventListener('mouseout',function(){
+    headOne.textContent = "HOVER OVER ME";
+    headOne.style.color = 'black';
+})
